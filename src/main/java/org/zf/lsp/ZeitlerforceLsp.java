eb5bdf0fcbdd044d33dc755cbc165562acf4575d
Cody T. Zeitler <codyzeitler12@gmail.com>
2025-01-20 21:29:03 -0700

    feat: initial commit

---
+++ src/main/java/org/zf/lsp/ZeitlerforceLsp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.zf.lsp;
+
+/**
+ *
+ * @author Cody
+ */
+public class ZeitlerforceLsp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
